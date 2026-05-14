ea9b50490e3132581a650683764f91e07747fca1
aiman <aimanhaikal312@gmail.com>
2026-05-14 11:01:07 +0800

    updated new design

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,37 @@
 
     <!--bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .grid-box {
+            display: grid;
+            grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+            gap: 1rem;
+        }
+
+            .card {
+                background-color: rgb(247, 243, 189);
+            }
+
+            .card:hover {
+                background-color: rgb(190, 186, 118);
+                transform: scale(1.05);
+                transition: transform 0.3s ease;
+            }
+
+            .card:active {
+                background-color: rgb(190, 186, 118);
+                transform: scale(0.99);
+                transition: transform 0.3s ease;
+            }
+
+            .card-body{
+                text-color: black;
+                font-size: 1.5rem;
+                align-items: center;
+                justify-content: center;
+                display: flex;
+            }
+    </style>
 
     <title>Attendance System</title>
 </head>
@@ -15,18 +46,19 @@
             @csrf
             <div class="mb-3">
                 <label for="class" class="form-label">Class Name</label>
-                <input type="text" class="form-control" id="class" name="class" placeholder="Enter class name">
+                <input type="text" class="form-control" id="class" name="class" placeholder="Enter class name" required>
             </div>
             <button type="submit" class="btn btn-primary">Add Class</button>
         </form>
 
+        <!--class list-->
         <div class="grid-box">
             <div class="row row-cols-1 row-cols-md-3 g-4 mt-3">
                 @foreach ($data as $item)
                     <div class="col">
                         <div class="card h-100">
                             <div class="card-body">
-                                <h5 class="card-title">{{ $item->Classes }}</h5>
+                                <a href="#" class="card-title stretched-link text-decoration-none">{{ $item->Classes }}</a>
                             </div>
                         </div>
                     </div>
